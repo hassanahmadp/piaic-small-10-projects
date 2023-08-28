0c7ee792fb8e693c631372678e21bcfabf7b5332
hassanahmadp <hassanahmadp@gmail.com>
2023-08-28 05:58:41 +0500

    client server bug needs to be solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from 'next/image'
+"use client"
+
+import { useBrowserStorage } from "@/utils"
+import { twMerge } from "tailwind-merge"
+
+export const dynamic = "force-dynamic"
 
 export default function Home() {
+  debugger
+  const [theme, setTheme] = useBrowserStorage<1 | 2 | 3>("theme", 1)
+  const theme1 = theme === 1
+  const theme2 = theme === 2
+  const theme3 = theme === 3
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main
+      className={twMerge(
+        "flex justify-center items-center min-h-screen",
+        theme1 && "bg-theme1_bg_main",
+        theme2 && "bg-theme2_bg_main",
+        theme3 && "bg-theme3_bg_main",
+      )}
+    >
+      <div className="w-full max-w-[580px] mx-auto px-5 py-8">
+        <div className="flex justify-between items center">
+          <h1>calc</h1>
+          <span>switch</span>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ src/utils/hooks/useBrowserStorage.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import React, { useEffect, useState } from "react"
+
+
+export function useBrowserStorage<T>(
+  key: string,
+  value: T,
+  storageType: "local" | "session" = "local",
+) {
+
+  const [state, setState] = useState<T | undefined>(() => {
+    if(typeof window !== "undefined") {
+      const storageItem =  storageType === "local" ? localStorage.getItem(key) : sessionStorage.getItem(key)
+      if (!storageItem) return value
+      return JSON.parse(storageItem)
+    } else return undefined
+  })
+
+  useEffect(() => {
+    if (typeof window === "undefined") return
+    if (storageType === "local") {
+      localStorage.setItem(key, JSON.stringify(state))
+    } else {
+      sessionStorage.setItem(key, JSON.stringify(state))
+    }
+  }, [state])
+
+  return [state, setState]
+}
